b509c2109de2aaa710e9f81a7834bb460ff7a47f
Gabriel Keith <gabriel.keith@ncino.com>
2019-10-24 11:29:29 -0400

    Created common files.

---
+++ src/common/data/orgs.ts
@@ -0,0 +1,40 @@
+
+//sfdx force:org:list
+export interface OrgList {
+	nonScratchOrgs: SalesforceOrg[]
+	scratchOrgs: ScratchOrg[]
+}
+
+export interface SalesforceOrg {
+	username: string
+	orgId: string
+	accessToken: string
+	instanceUrl: string
+	loginUrl: string
+	clientId: string
+	alias: string
+	lastUsed: Date
+	connectedStatus: string
+}
+
+export interface ScratchOrg extends SalesforceOrg {
+	createdOrgInstance: string
+	created: string
+	devHubUsername: string
+	connectedStatus: string
+	attributes: ScratchOrgAttributes
+	orgName: string
+	status: string
+	createdBy: string
+	createdDate: string
+	expirationDate: Date
+	edition: string
+	signupUsername: string
+	devHubOrgId: string
+	isExpired: boolean
+}
+
+export interface ScratchOrgAttributes {
+	type: string
+	url: string
+}
