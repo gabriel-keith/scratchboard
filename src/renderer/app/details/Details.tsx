c14b2079da81d4cc36f171facd8f775748645eea
BrittonDeets <britton.deets@ncino.com>
2019-10-24 14:32:09 -0400

    adding tailwind css

--- src/renderer/app/details/Details.tsx
+++ src/renderer/app/details/Details.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export class Details extends React.Component {
 	public render() {
-		return '';
+		return (
+			<div className="flex bg-teal-100 text-gray-800">
+				<p>Hello Tailwind!</p>
+			</div>
+		);
 	}
 }
