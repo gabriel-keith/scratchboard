9bbce082f439bc6329fb8ae283ca388b8884c3d0
BrittonDeets <britton.deets@ncino.com>
2019-10-24 15:06:17 -0400

    adding details html

--- src/renderer/app/details/Details.tsx
+++ src/renderer/app/details/Details.tsx
@@ -1,11 +1,56 @@
 import React from 'react';
+import { Button, Card, Elevation, ProgressBar } from "@blueprintjs/core";
+ 
 
 export class Details extends React.Component {
-	public render() {
-		return (
-			<div className="flex bg-teal-100 text-gray-800">
-				<p>Hello Tailwind!</p>
-			</div>
-		);
-	}
+
+    render() {
+        return (
+			<Card interactive={false} elevation={Elevation.ONE} className="py-2 px-5">
+				<h3 className="py-2 mb-4 ml-3 text-lg">Details</h3>
+				<div className="flex flex-wrap justify-between ml-3 w-3/4 mb-4">
+					<div className="p-1">
+						<p>Deployment Status</p>
+						<ProgressBar animate={false} stripes={false} value={.5} 
+						className="bp3-intent-primary my-2"></ProgressBar>
+					</div>
+					<div className="p-1">
+						<p>Username</p>
+						<p className="ml-3">User</p>
+					</div>
+					<div className="p-1">
+						<p>Dependencies</p>
+						<p className="ml-3"> View List</p>
+					</div>
+				</div>
+				<div className="flex flex-wrap justify-between ml-3 w-3/4 mb-4">
+					<div className="p-1">
+						<p>Packages Installed</p>
+						<p className="ml-3">Value</p>
+					</div>
+					<div className="p-1">
+						<p>Password</p>
+						<p className="ml-3">*********</p>
+					</div>
+					<div className="p-1">
+						<p>Users</p>
+						<p className="ml-3">View List</p>
+					</div>
+				</div>
+				<div className="flex flex-wrap justify-between ml-3 w-3/4 mb-4">
+					<div className="p-1">
+						<p>Frontdoor URL</p>
+						<p className="ml-3">https://www.scratchboard.com/is/awesome.html</p>
+					</div>
+					<div className="p-1">
+						<p>Versions</p>
+						<p className="ml-3">2.3456</p>
+					</div>
+					<div className="p-1">
+						<p></p>
+						<p className="ml-3"></p>
+					</div>
+				</div>
+			</Card>);
+    }
 }
