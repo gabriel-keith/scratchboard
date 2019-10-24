66a4d8bc896fd39d4c5c29c680208f53cf5a27ee
BrittonDeets <britton.deets@ncino.com>
2019-10-24 17:01:30 -0400

    adding dependencies

--- src/renderer/app/actions/Actions.tsx
+++ src/renderer/app/actions/Actions.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Tab, Tabs } from '@blueprintjs/core';
+import { Card, Tab, Tabs, HTMLTable, Checkbox, Alignment } from '@blueprintjs/core';
 
 export class Actions extends React.Component {
 	constructor(props) {
@@ -48,9 +48,44 @@ export class TerminalPanel extends React.Component {
 export class DependenciesPanel extends React.Component { 
 	public render() {
 		return (
-		<div className="bg-yellow-500">
-			<p>Dependencies Works!</p>
-		</div>);
+			<Card id="dependencies" interactive={false} className="m-2">
+				<HTMLTable bordered={false} interactive={true}>
+					<thead>
+						<tr>
+							<th>Package</th>
+							<th>Installed Version</th>
+							<th>Remote Version</th>
+							<th>Upgrade to remote</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>LLC_BI</td>
+							<td>2.9354</td>
+							<td>2.9765</td>
+							<td>
+								<Checkbox alignIndicator={Alignment.CENTER} checked={false} onChange={() => console.log(':)')} />
+							</td>
+						</tr>
+						<tr>
+							<td>nFORCE</td>
+							<td>1.8234</td>
+							<td>1.9456</td>
+							<td>
+								<Checkbox checked={false} onChange={() => console.log(':)')} />
+							</td>
+						</tr>
+						<tr>
+							<td>nDESIGN</td>
+							<td>7.532</td>
+							<td>7.975</td>
+							<td>
+								<Checkbox checked={false} onChange={() => console.log(':)')} />
+							</td>
+						</tr>
+					</tbody>
+				</ HTMLTable>
+			</Card>);
 	}
 }
 
