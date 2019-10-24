ac1662c4c6305592b568d5eaa99a4ff9f7956905
BrittonDeets <britton.deets@ncino.com>
2019-10-24 16:36:35 -0400

    adding scripts panel and removing search

--- src/renderer/app/actions/Actions.tsx
+++ src/renderer/app/actions/Actions.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
-
-import { Card } from '@blueprintjs/core';
+import { Card, Tab, Tabs } from '@blueprintjs/core';
 
 export class Actions extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedTabId: 'sp'
+	  }
+	}
+
+	setSelectedTab(selectedTabId) {
+		this.setState({selectedTabId: selectedTabId});
+	}
+
 	public render() {
-		return <Card id="sidebar" className="m-4">Test Actions</Card>;
+		return (
+			<Card id="sidebar" className="m-4">
+				<Tabs id="tabs" onChange={(selectedTabId) => this.setSelectedTab(selectedTabId)} selectedTabId={this.state.selectedTabId}>
+					<Tab id="stdp" title="Standard Actions" panel={<StandardActionPanel />} />
+					<Tab id="trmp" title="Terminal" panel={<TerminalPanel />} />
+					<Tab id="scrp" title="Scripts" panel={<ScriptsPanel />} />
+				</Tabs>
+			</Card>
+		);
+	}
+}
+
+export class StandardActionPanel extends React.Component { 
+	public render() {
+		return (
+		<div className="bg-green-500">
+			<p>Panel Works!</p>
+		</div>);
+	}
+}
+
+export class TerminalPanel extends React.Component { 
+	public render() {
+		return (
+		<div className="bg-blue-500">
+			<p>Terminal Works!</p>
+		</div>);
+	}
+}
+
+export class ScriptsPanel extends React.Component { 
+	public render() {
+		return (
+		<div className="bg-red-500">
+			<p>Scripts Works!</p>
+		</div>);
 	}
 }
