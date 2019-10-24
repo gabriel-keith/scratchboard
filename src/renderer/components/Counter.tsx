93e2a3736ec09b3dbf5551b88c1c1230d96983b9
Karen Dana <karen.dana@ncino.com>
2019-10-24 11:37:00 -0400

    Add tslint

--- src/renderer/components/Counter.tsx
+++ src/renderer/components/Counter.tsx
@@ -1,41 +1,45 @@
-import React from 'react'
-import { Button } from '@blueprintjs/core'
+import React from 'react';
+import { Button } from '@blueprintjs/core';
 
-//Just an example component to mess around with
+// Just an example component to mess around with
 
 export interface CounterProps {
-	increment: number,
-	handleClick: () => void
+	increment: number;
+	handleClick: () => void;
 }
 
 export interface CounterState {
-	count: number
+	count: number;
 }
 
 export class Counter extends React.Component<CounterProps, CounterState> {
-	constructor(props: CounterProps) {
-		super(props)
+	public constructor(props: CounterProps) {
+		super(props);
 
 		this.handleIncrement = this.handleIncrement.bind(this);
 
 		this.state = {
 			count: 0
-		}
+		};
 	}
 
-	handleIncrement() {
+	public handleIncrement() {
 		this.setState({
 			...this.state,
 			count: this.state.count + this.props.increment
-		})
+		});
 
 		this.props.handleClick();
 	}
 
-	render() {
-		return <div className="sb-counter">
-			<div>Count: {this.state.count}</div>
-			<Button onClick={this.handleIncrement}>Increment by {this.props.increment}</Button>
-		</div>
+	public render() {
+		return (
+			<div className='sb-counter'>
+				<div>Count: {this.state.count}</div>
+				<Button onClick={this.handleIncrement}>
+					Increment by {this.props.increment}
+				</Button>
+			</div>
+		);
 	}
 }
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
 
-import { App } from './app/App'
+import { App } from './app/App';
 
-import './style.scss'
+import './style.scss';
 
 ReactDom.render(
 	<div className='sb-app bp3-dark'>
 		<App />
 	</div>,
 	document.getElementById('app')
-)
+);
