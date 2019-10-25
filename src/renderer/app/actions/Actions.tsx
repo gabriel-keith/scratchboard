4de6fa7b0609548cb239f0dbaa8365426d2f1658
BrittonDeets <britton.deets@ncino.com>
2019-10-25 09:26:22 -0400

    renaming panel to tab

--- src/renderer/app/actions/Actions.tsx
+++ src/renderer/app/actions/Actions.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Tab, Tabs, HTMLTable, Checkbox, Alignment } from '@blueprintjs/core';
+import { DependenciesTab } from './DependenciesTab';
 
 export class Actions extends React.Component {
 	constructor(props) {
@@ -17,17 +18,17 @@ export class Actions extends React.Component {
 		return (
 			<Card id="sidebar" className="m-4">
 				<Tabs id="tabs" onChange={(selectedTabId) => this.setSelectedTab(selectedTabId)} selectedTabId={this.state.selectedTabId}>
-					<Tab id="stdp" title="Standard Actions" panel={<StandardActionPanel />} />
-					<Tab id="dpcp" title="Dependencies" panel={<DependenciesPanel />} />
-					<Tab id="trmp" title="Terminal" panel={<TerminalPanel />} />
-					<Tab id="scrp" title="Scripts" panel={<ScriptsPanel />} />
+					<Tab id="stdp" title="Standard Actions" panel={<StandardActionTab />} />
+					<Tab id="dpcp" title="Dependencies" panel={<DependenciesTab  />} />
+					<Tab id="trmp" title="Terminal" panel={<TerminalTab  />} />
+					<Tab id="scrp" title="Scripts" panel={<ScriptsTab  />} />
 				</Tabs>
 			</Card>
 		);
 	}
 }
 
-export class StandardActionPanel extends React.Component { 
+export class StandardActionTab extends React.Component { 
 	public render() {
 		return (
 		<div className="bg-green-500">
@@ -36,7 +37,7 @@ export class StandardActionPanel extends React.Component {
 	}
 }
 
-export class TerminalPanel extends React.Component { 
+export class TerminalTab extends React.Component { 
 	public render() {
 		return (
 		<div className="bg-blue-500">
@@ -45,51 +46,7 @@ export class TerminalPanel extends React.Component {
 	}
 }
 
-export class DependenciesPanel extends React.Component { 
-	public render() {
-		return (
-			<Card id="dependencies" interactive={false} className="m-2">
-				<HTMLTable bordered={false} interactive={true}>
-					<thead>
-						<tr>
-							<th>Package</th>
-							<th>Installed Version</th>
-							<th>Remote Version</th>
-							<th>Upgrade to remote</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>LLC_BI</td>
-							<td>2.9354</td>
-							<td>2.9765</td>
-							<td>
-								<Checkbox alignIndicator={Alignment.CENTER} checked={false} onChange={() => console.log(':)')} />
-							</td>
-						</tr>
-						<tr>
-							<td>nFORCE</td>
-							<td>1.8234</td>
-							<td>1.9456</td>
-							<td>
-								<Checkbox checked={false} onChange={() => console.log(':)')} />
-							</td>
-						</tr>
-						<tr>
-							<td>nDESIGN</td>
-							<td>7.532</td>
-							<td>7.975</td>
-							<td>
-								<Checkbox checked={false} onChange={() => console.log(':)')} />
-							</td>
-						</tr>
-					</tbody>
-				</ HTMLTable>
-			</Card>);
-	}
-}
-
-export class ScriptsPanel extends React.Component { 
+export class ScriptsTab extends React.Component { 
 	public render() {
 		return (
 		<div className="bg-red-500">
---
+++ src/renderer/app/actions/DependenciesTab.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Card, HTMLTable, Checkbox, Alignment } from '@blueprintjs/core';
+
+export class DependenciesTab extends React.Component { 
+	public render() {
+		return (
+			<Card id="dependencies" interactive={false} className="m-2">
+				<HTMLTable bordered={false} interactive={true}>
+					<thead>
+						<tr>
+							<th>Package</th>
+							<th>Installed Version</th>
+							<th>Remote Version</th>
+							<th>Upgrade to remote</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>LLC_BI</td>
+							<td>2.9354</td>
+							<td>2.9765</td>
+							<td>
+								<Checkbox alignIndicator={Alignment.CENTER} checked={false} onChange={() => console.log(':)')} />
+							</td>
+						</tr>
+						<tr>
+							<td>nFORCE</td>
+							<td>1.8234</td>
+							<td>1.9456</td>
+							<td>
+								<Checkbox checked={false} onChange={() => console.log(':)')} />
+							</td>
+						</tr>
+						<tr>
+							<td>nDESIGN</td>
+							<td>7.532</td>
+							<td>7.975</td>
+							<td>
+								<Checkbox checked={false} onChange={() => console.log(':)')} />
+							</td>
+						</tr>
+					</tbody>
+				</ HTMLTable>
+			</Card>);
+	}
+}
