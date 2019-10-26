87ad2b7d9f31803aab77dd8e273b1122373b0f02
Gabriel Keith <gabriel.keith@ncino.com>
2019-10-26 15:03:52 -0400

    Titlebar: Double click behavior

--- src/renderer/app/Titlebar.tsx
+++ src/renderer/app/Titlebar.tsx
@@ -1,10 +1,31 @@
+import { remote } from 'electron';
 import React from 'react';
 
 export class TitleBar extends React.Component {
+	constructor(props: {}) {
+		super(props);
+
+		this.handleDoubleClick = this.handleDoubleClick.bind(this);
+	}
+
 	public render() {
-		return (
-			<div className='sb-title-bar'>
-			</div>
+		return <div className='sb-title-bar' onDoubleClick={this.handleDoubleClick} ></div>;
+	}
+
+	private handleDoubleClick() {
+		const doubleClickAction = remote.systemPreferences.getUserDefault(
+			'AppleActionOnDoubleClick',
+			'string'
 		);
+		const win = remote.getCurrentWindow();
+		if (doubleClickAction === 'Minimize') {
+			win.minimize();
+		} else if (doubleClickAction === 'Maximize') {
+			if (!win.isMaximized()) {
+				win.maximize();
+			} else {
+				win.unmaximize();
+			}
+		}
 	}
 }
