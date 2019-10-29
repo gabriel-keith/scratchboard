8137fd55aa6057ebed18bc5ad62977499e34b9c7
BrittonDeets <britton.deets@ncino.com>
2019-10-29 09:44:46 -0400

    details styling changes

--- src/renderer/app/details/Details.tsx
+++ src/renderer/app/details/Details.tsx
@@ -19,7 +19,7 @@ export class Details extends React.Component<Props> {
 			>
 				<h3 className='py-2 mb-4 ml-3 text-lg'>Details</h3>
 				<div className='flex flex-wrap justify-between ml-3 w-3/4 mb-4'>
-					<div className='p-1 w-full lg:mx-1 md:w-full lg:w-5/12'>
+					{/* <div className='p-1 w-full lg:mx-1 md:w-full lg:w-5/12'>
 						<p>Deployment Status</p>
 						<ProgressBar
 							animate={false}
@@ -27,8 +27,8 @@ export class Details extends React.Component<Props> {
 							value={0.5}
 							className='bp3-intent-primary my-2'
 						></ProgressBar>
-					</div>
-					<div className='p-1 w-full lg:mx-1 md:w-full lg:w-5/12'>
+					</div> */}
+					<div className='p-1 w-full'>
 						<p>Expiration</p>
 						<ProgressBar
 							animate={false}
@@ -39,30 +39,30 @@ export class Details extends React.Component<Props> {
 						<p>{this.getDaysToExpiration()} Days Left</p>
 					</div>
 				</div>
-				<div className='flex flex-wrap justify-between ml-3 w-3/4 mb-4'>
-					<div className='p-1'>
+				<div className='flex flex-wrap justify-between ml-3 w-full mb-4'>
+					<div className='p-1 w-1/3'>
 						<p>Username</p>
 						<p className='ml-3'>{this.props.scratchOrg.username}</p>
 					</div>
-					<div className='p-1'>
+					<div className='p-1 w-1/3'>
 						<p>Created</p>
 						<p className='ml-3'>{new Date(this.props.scratchOrg.createdDate).toISOString().split('T')[0]}</p>
 					</div>
-					<div className='p-1'>
+					<div className='p-1 w-1/3'>
 						<p>Expires</p>
 						<p className='ml-3'>{this.props.scratchOrg.expirationDate}</p>
 					</div>
 				</div>
-				<div className='flex flex-wrap justify-between ml-3 w-3/4 mb-4'>
-					<div className='p-1'>
+				<div className='flex flex-wrap justify-between ml-3 w-full mb-4'>
+					<div className='p-1 w-1/3'>
 						<p>Connected Status</p>
 						<p className='ml-3'>{this.props.scratchOrg.connectedStatus}</p>
 					</div>
-					<div className='p-1'>
+					<div className='p-1 w-1/3'>
 						<p>Org Name</p>
 						<p className='ml-3'>{this.props.scratchOrg.orgName}</p>
 					</div>
-					<div className='p-1'>
+					<div className='p-1 w-1/3'>
 						<p>Org Id</p>
 						<p className='ml-3'>{this.props.scratchOrg.devHubOrgId}</p>
 					</div>
@@ -84,7 +84,7 @@ export class Details extends React.Component<Props> {
 		const totalTime = Math.ceil(difference / (1000 * 3600 * 24));
 		const timeLeft = this.getDaysToExpiration();
 
-		const remainingPercent = timeLeft/totalTime;
+		const remainingPercent = 1-(timeLeft/totalTime);
 		return remainingPercent;
 	}
 }
