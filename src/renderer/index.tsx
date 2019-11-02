841c9dd7872f9741d113f45f64a30cbd7b3dbcd6
Gabriel Keith <gabriel.keith@ncino.com>
2019-11-02 19:13:50 -0400

    Init fix

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -5,8 +5,9 @@ import { Provider } from 'react-redux';
 
 import App from './app/App';
 
+import 'tailwindcss/dist/base.css';
+import 'tailwindcss/dist/utilities.css';
 import './style.scss';
-import './tailwind.scss';
 
 ReactDom.render(
 	<Provider store={store}>
